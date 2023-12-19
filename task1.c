dd98717c1bbb59fb5b5fb466720d9f1076341d35
Harsh-Mangukiya <mangukiyah111@gmail.com>
2023-12-19 17:36:16 +0530

    lec 10.

---
+++ task1.c
@@ -0,0 +1,21 @@
+/*
+Q.1 Write a Program to find the cube of a given number using UDF.For example,
+Input:
+Enter any number: 5
+
+Output:
+Cube is: 125
+*/
+#include<stdio.h>
+	void dhruv(){
+		int n;
+		
+		printf("enter any value :-");
+		scanf("%d",&n);
+		
+		printf("sum of cube :---%d",n*n*n);
+	}
+	main(){
+		
+		dhruv();
+	}
